507185b21ccf6b370aeb1afeb746fa7125677d6e
mbelen <beel.betanzoo@hotmail.com.ar>
2024-09-19 23:11:20 -0300

    home y navbar - Belen T2

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,21 +4,38 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
-      <div className="flex justify-center">
-        <img  src="/assets/lasercol-logo.png" alt="LaserCol Logo" />
+    <div>
+      <div className="flex justify-center mb-4 mt-4">
+        <img src="/assets/lasercol-logo.png" className='lg:w-[150px] lg:h-[50px]' alt="LaserCol Logo" />
       </div>
-      <ul className="flex justify-evenly pt-2 pb-2 bg-[#fce6f2]">
-          <li className="font-sans text-[24px] font-normal leading-[30.17px] text-left">
-            <Link href="/">Home</Link></li>
-          <li className="font-sans text-[24px] font-normal leading-[30.17px] text-left">
-            <Link href="/products">Productos</Link></li>
-          <li className="font-sans text-[24px] font-normal leading-[30.17px] text-left">
-            <Link href="/about">Sobre Nosotros</Link></li>
-          <li className="font-sans text-[24px] font-normal leading-[30.17px] text-left">
-            <Link href="/desings">Diseños</Link></li>
-        </ul>
+      <nav className="bg-gradient-to-r from-pink-100 via-purple-100 to-yellow-100 py-4 px-8">
+      <div className="flex justify-between items-center">
+        <div className="flex space-x-8">
+          <Link href="/" className="text-black hover:text-gray-600 text-lg">Home</Link>
+          <Link href="/products" className="text-black hover:text-gray-600 text-lg">Productos</Link>
+          <Link href="/about" className="text-black hover:text-gray-600 text-lg">Sobre nosotros</Link>
+          <Link href="/designs" className="text-black hover:text-gray-600 text-lg">Diseños</Link>
+        </div>
+        <div className="flex items-center space-x-2 bg-gray-100 rounded-full px-4 py-2">
+          
+          <input 
+            type="text" 
+            placeholder="Busca tus productos favoritos" 
+            className="bg-transparent outline-none text-gray-700 placeholder-gray-500 w-[250px]" 
+          />
+          
+        </div>
+        <div className="flex space-x-4">
+          <Link href="/midashboard">
+            <img src="/assets/iconouser.png" className='mb-[2px]' alt="icono carrito" />
+          </Link>
+          <Link href="/cart">
+            <img src="/assets/iconocarrito.png" alt="icono usuario" />
+          </Link>
+        </div>
+      </div>
     </nav>
+    </div>
   )
 }
 
