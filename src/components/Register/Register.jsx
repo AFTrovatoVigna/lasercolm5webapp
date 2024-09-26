e282f34e056e26a4e74799200ed4ebe2fb337d75
mbelen <beel.betanzoo@hotmail.com.ar>
2024-09-26 14:18:04 -0300

    Registro y login T10 y T11 Belen

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,7 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { validateRegister } from '@/helpers/validateRegister';
-
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { register } from '@/helpers/auth.helper';
 import Swal from 'sweetalert2';
@@ -15,144 +12,234 @@ const RegisterForm = () => {
     name: '',
     email: '',
     password: '',
+    passwordConfirmation: '',
+    Dni: '',
     address: '',
-    phone: ''
-  }
+    phone: '',
+    country: '',
+    city: '',
+    birthDate: ''
+  };
 
-  const [dataUser, setDataUser] = useState(initialState)
-  const [errors, setErrors] = useState(initialState)
+  const [dataUser, setDataUser] = useState(initialState);
+  const [errors, setErrors] = useState(initialState);
 
   const handleChange = (event) => {
-    const {name, value} = event.target;
-    setDataUser({...dataUser, [name]: value})
+    const { name, value } = event.target;
+    setDataUser({ ...dataUser, [name]: value });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
-      await register(dataUser)
-      Swal.fire("Te registraste correctamente") 
-      router.push("/login")
-    } 
 
-    catch (error) {
-        Swal.fire("No pudimos registrarte") 
-      throw new Error(error)
-      
+    // Validaciones antes de enviar los datos
+    const validationErrors = validateRegister(dataUser);
+    setErrors(validationErrors);
+
+    // Si hay errores, no continuar con el registro
+    if (Object.keys(validationErrors).length > 0) {
+      return;
     }
-    };
 
-    useEffect(() => {
-      const errors = validateRegister(dataUser)
-      setErrors(errors)
-     }, [dataUser])
+    try {
+      await register(dataUser);
+      Swal.fire("Te registraste correctamente");
+      router.push("/login");
+    } catch (error) {
+      Swal.fire("No pudimos registrarte");
+      throw new Error(error);
+    }
+  };
 
-    
+  useEffect(() => {
+    const errors = validateRegister(dataUser);
+    setErrors(errors);
+  }, [dataUser]);
 
-     return (
-        <div className="relative w-full h-screen p-8"> 
-          <video
-            src="/assets/videologinregister2.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="absolute inset-0 object-cover w-full h-full"
-          >
-            Your browser does not support the video tag.
-          </video>
-      
-          <div className="absolute inset-0 flex items-center justify-center">
-            <form 
-              onSubmit={handleSubmit} 
-              className="w-full max-w-md lg:max-w-lg shadow-lg mt-40 mb-20 p-6 bg-gradient-to-r from-pink-200 via-pink-200 to-pink-200 text-black rounded-xl max-h-[90%] overflow-hidden" 
-              style={{ maxHeight: '110vh' }} 
-            >
-              <h2 className="text-xl font-bold mb-4 text-center">Registrate acá</h2>
-      
-              <div className="mb-2">
-                <label htmlFor="name" className="block mb-1">Nombre</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={dataUser.name}
-                  onChange={handleChange}
-                  className="w-full p-2 h-9 bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
-                  required
-                />
-                {errors.name && <p className="text-red-500">{errors.name}</p>}
-              </div>
-      
-              <div className="mb-2">
-                <label htmlFor="email" className="block mb-1">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={dataUser.email}
-                  onChange={handleChange}
-                  className="w-full p-2 h-9 bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
-                  required
-                />
-                {errors.email && <p className="text-red-500">{errors.email}</p>}
-              </div>
-      
-              <div className="mb-2">
-                <label htmlFor="password" className="block mb-1">Contraseña</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={dataUser.password}
-                  onChange={handleChange}
-                  className="w-full p-2 h-9 bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
-                  required
-                />
-                {errors.password && <p className="text-red-500">{errors.password}</p>}
-              </div>
-      
-              <div className="mb-2">
-                <label htmlFor="address" className="block mb-1">Dirección</label>
-                <input
-                  type="text"
-                  id="address"
-                  name="address"
-                  value={dataUser.address}
-                  onChange={handleChange}
-                  className="w-full p-2 h-9 bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
-                  required
-                />
-                {errors.address && <p className="text-red-500">{errors.address}</p>}
-              </div>
-      
-              <div className="mb-2">
-                <label htmlFor="phone" className="block mb-1">Teléfono</label>
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  value={dataUser.phone}
-                  onChange={handleChange}
-                  className="w-full p-2 h-9 bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
-                  required
-                />
-                {errors.phone && <p className="text-red-500">{errors.phone}</p>}
-              </div>
-      
-              <button 
-                type="submit" 
-                disabled={!!errors.email} 
-                className="w-full p-2 bg-pink-500 text-black rounded-lg mt-5 font-semibold hover:bg-pink-700 transition-colors"
-              >
-                Register
-              </button>
-            </form>
+  return (
+    <div className="relative w-full h-screen p-8">
+      <video
+        src="/assets/videologinregister2.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute inset-0 object-cover w-full h-full"
+      >
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md lg:max-w-lg shadow-xl  mt-40 mb-20 p-6 bg-pink-200 rounded-xl text-black  grid grid-cols-1 gap-4 lg:grid-cols-2"
+        >
+          <h2 className="text-2xl  mb-4 col-span-2 text-center">Regístrate acá</h2>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="name" className="block  mb-1">Nombre</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Ingresa tu nombre" 
+              value={dataUser.name}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.name && <p className="text-red-500">{errors.name}</p>}
           </div>
-        </div>
-      );
-      
-      
 
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="email" className="block  mb-1">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Ingresa tu email✉️" 
+              value={dataUser.email}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="password" className="block  mb-1">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Ingresa tu contraseña🔑" 
+              value={dataUser.password}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.password && <p className="text-red-500">{errors.password}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="passwordConfirmation" className="block mb-1">Confirmar Contraseña</label>
+            <input
+              type="password"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+              placeholder="Confirma tu contraseña🔑"
+              value={dataUser.passwordConfirmation}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.passwordConfirmation && <p className="text-red-500">{errors.passwordConfirmation}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="Dni" className="block mb-1">Ingresa tu DNI</label>
+            <input
+              type="text"
+              id="Dni"
+              name="Dni"
+              placeholder="Ingresa tu DNI" 
+              value={dataUser.Dni}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.Dni && <p className="text-red-500">{errors.Dni}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="address" className="block mb-1">Dirección</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Ingresa tu dirección" 
+              value={dataUser.address}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.address && <p className="text-red-500">{errors.address}</p>}
+          </div>
+
+         
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="phone" className="block mb-1">Telefono</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="Ingresa tu telefono" 
+              value={dataUser.phone}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.phone && <p className="text-red-500">{errors.phone}</p>}
+          </div>
+
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="country" className="block  mb-1">País de residencia</label>
+            <input
+              type="text"
+              id="country"
+              name="country"
+              placeholder="Ingresa tu país" 
+              value={dataUser.country}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.country && <p className="text-red-500">{errors.country}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="city" className="block  mb-1">Ciudad</label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              placeholder="Ingresa tu ciudad🏠" 
+              value={dataUser.city}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.city && <p className="text-red-500">{errors.city}</p>}
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <label htmlFor="birthDate" className=" block mb-1">Fecha de nacimiento</label>
+            <input
+              type="date" // Cambiado a "date" para un formato más adecuado
+              id="birthDate"
+              name="birthDate"
+              placeholder="Ingresa tu fecha de nacimiento" 
+              value={dataUser.birthDate}
+              onChange={handleChange}
+              className="w-full p-2 h-9 shadow-md bg-white border text-black rounded-lg focus:outline-none focus:border-pink-900"
+              required
+            />
+            {errors.birthDate && <p className="text-red-500">{errors.birthDate}</p>}
+          </div>
+
+          <button
+            type="submit"
+            className="col-span-2 p-2 h-12 mt-4 text-xl font-bold text-white bg-pink-600 rounded-lg shadow-md hover:bg-pink-500 focus:outline-none"
+          >
+            Registrarse
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
+
 export default RegisterForm;
+
--- src/helpers/validateRegister.js
+++ src/helpers/validateRegister.js
@@ -6,12 +6,22 @@ export const validateRegister = (form) => {
   const errors = {};
 
   const regExp = {
-    name: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,50}$/,
-    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    password: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
-    address: /^[a-zA-Z0-9\s,.'-]{3,}$/,
-    phone: /^\+?\d{1,3}?[-.\s]?\(?\d{1,4}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
+
+    name: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/, // Permite letras y caracteres acentuados pero NO espacios
+
+      email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, // Formato de email
+      password: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
+      passwordConfirmation: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
+      Dni: /^[\d]{1,3}\.?[\d]{3,3}\.?[\d]{3,3}$/, // Ajustar según el formato del DNI que necesites
+      address: /^[a-zA-Z0-9\s,.'-]{3,}$/,
+      phone: /^\d{10,15}$/, // Permite solo dígitos, ajuste el rango según sea necesario
+
+      country:/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,50}$/,
+      city:/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,50}$/,
+      birthDate:/^\d{4}-\d{2}-\d{2}$/
   };
+  
+
 
   if (form.name && !regExp.name.test(form.name)) {
     errors.name = "❌Ingresa un nombre válido";
@@ -25,12 +35,32 @@ export const validateRegister = (form) => {
     errors.password = "❌Ingresa una contraseña válida";
   }
 
+  if (form.passwordConfirmation && !regExp.passwordConfirmation.test(form.passwordConfirmation)) {
+    errors.passwordConfirmation = "❌Ingresa una contraseña válida";
+  }
+
+  if (form.Dni && !regExp.Dni.test(form.Dni)) {
+    errors.Dni = "❌Ingresa una dirección válida";
+  }
+
   if (form.address && !regExp.address.test(form.address)) {
     errors.address = "❌Ingresa una dirección válida";
   }
 
-  if (form.phone && !regExp.phone.test(form.phone)) {
+  if (form.phone && !regExp.phone.test(form.phone.toString())) {
     errors.phone = "❌Ingresa un número de teléfono válido";
+  }
+
+  if (form.country && !regExp.country.test(form.country)) {
+    errors.country = "❌Ingresa un paìs válido";
+  }
+
+  if (form.city && !regExp.city.test(form.city)) {
+    errors.city = "❌Ingresa una ciudad válida";
+  }
+
+  if (form.birthDate && !regExp.birthDate.test(form.birthDate)) {
+    errors.birthDate = "❌Ingresa una fecha válida (YYYY-MM-DD)";
   }
 
   return errors;
