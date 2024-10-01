ceb54ff2000552ffc30717b39c6bb7f27f330e34
Galahamd <galahamd@hotmail.com>
2024-10-01 16:23:05 -0400

    Usin APIURL

--- src/helpers/auth.helper.js
+++ src/helpers/auth.helper.js
@@ -16,7 +16,7 @@ export async function register(userData) {
     userData.phone = Number(userData.phone); // Asegúrate de que sea un número
 
     try {
-        const res = await fetch(`${APIURL}/register`, {
+        const res = await fetch(`${APIURL}/auth/register`, {
             method: `POST`,
             headers: {
                 "Content-type": "application/json"
