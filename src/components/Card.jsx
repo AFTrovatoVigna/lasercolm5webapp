a9450ba856d66171a67f0a143550987e49ff94b2
dayana gomez <dayagomez31@hotmail.com>
2024-10-02 17:21:08 -0500

    avance popup producto

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,23 @@
+// components/Card.js
 import Link from "next/link";
 import Image from "next/image";
-function ProductCard({ product }) {
+
+function ProductCard({ product, onQuickBuy }) { 
     return (
-        <div className="flex flex-col items-center text-center ">
+        <div className="flex flex-col items-center text-center">
             <Link href={`/detail/${product.id}`} passHref>
-                
-            <h1>{product.nombre} </h1>
-            <p>{product.valor}
-            <Image src={product.imgUrl} alt={product.name} width={250} height={250} className="m-auto my-16" />
-            </p>
+                <h1>{product.nombre}</h1>
+                <p>{product.valor}</p>
+                <Image src={product.imgUrl} alt={product.nombre} width={250} height={250} className="m-auto my-16" />
             </Link>
+
+          
+            <button 
+                onClick={() => onQuickBuy(product)} 
+                className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600"
+            >
+                Compra Rápida
+            </button>
         </div>
     );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,29 +1,58 @@
-"use client"
+
+"use client";
 
 import { useEffect, useState } from "react";
 import ProductCard from "./Card";
+import ModalQuickBuy from "./ModalQuickBuy/ModalQuickBuy"; 
 
-function ProductGridComponent({products}) {
+function ProductGridComponent({ products }) {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [cartId, setCartId] = useState(null); 
 
-   
+
+    useEffect(() => {
+        const storedCartId = localStorage.getItem("cartId");
+        if (storedCartId) {
+            setCartId(storedCartId);
+        }
+    }, []);
+
+    
+    const handleModalOpen = (product) => {
+        setSelectedProduct(product);
+        setShowModal(true);
+    };
+
+  
+    const handleModalClose = () => {
+        setShowModal(false);
+        setSelectedProduct(null);
+    };
 
     return (
-<div className="">
+        <div className="">
+            <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-3">
+                {products.map((product) => (
+                    <ProductCard 
+                        product={product} 
+                        key={product.id} 
+                        onQuickBuy={handleModalOpen} 
+                    />
+                ))}
+            </div>
 
-        <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-3">
-            {products.map((product) => (
-                <ProductCard product={product} key={product.id} />
-
-
-            ))}
-
+           
+            {showModal && selectedProduct && (
+                <ModalQuickBuy
+                    show={showModal}
+                    onClose={handleModalClose}
+                    product={selectedProduct}
+                    cartId={cartId}
+                />
+            )}
         </div>
-    
-</div>
-
-
-    )
-
+    );
 }
 
 export default ProductGridComponent;
