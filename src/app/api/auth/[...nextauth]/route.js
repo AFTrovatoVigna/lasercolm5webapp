0175d193403a2516d823ab7285ffa66012912f25
mbelen <beel.betanzoo@hotmail.com.ar>
2024-10-02 00:41:24 -0300

    arreglando autenticacion

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,56 +1,7 @@
 import NextAuth from "next-auth/next";
 import Google from "next-auth/providers/google";
 
-
-
 const handler = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // Agregar scopes necesarios
-      authorization: {
-        params: {
-          scope: "profile email", // Agrega otros scopes si es necesario
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account, profile }) {
-      // Si es una sesión nueva, añade el accessToken y el Google ID (sub)
-      if (account) {
-        token.accessToken = account.access_token;
-        token.userId = profile.sub; // Guarda el Google ID único
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // Añadir el accessToken y userId (Google ID) a la sesión
-      session.accessToken = token.accessToken;
-      session.userId = token.userId; // Pasa el Google ID único a la sesión
-      return session;
-    },
-    async redirect({ url, baseUrl }) {
-      console.log("Redirect callback triggered:", { url, baseUrl });
-      if (url.includes("/api/auth/callback/google")) {
-        return `${baseUrl}/home`;
-      }
-      if (url === "/api/auth/signout") {
-        return baseUrl;
-      }
-      return baseUrl;
-    },
-  },
-  session: {
-    strategy: 'jwt',
-  },
-});
-
-export { handler as GET, handler as POST };
-
-
-/*const handler = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -87,9 +38,7 @@ export { handler as GET, handler as POST };
     strategy: 'jwt', 
   },
 });
-export { handler as GET, handler as POST };*/
-
-
+export { handler as GET, handler as POST };
 
 /*const handler = NextAuth({
   providers: [
