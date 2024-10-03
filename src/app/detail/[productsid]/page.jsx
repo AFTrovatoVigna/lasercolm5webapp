1f2d2b8c7dd68b0c3bee26fcb2a1391f9138b524
dayana gomez <dayagomez31@hotmail.com>
2024-10-02 19:27:52 -0500

    avance detalle

--- src/app/detail/[productsid]/page.jsx
+++ src/app/detail/[productsid]/page.jsx
@@ -1,32 +1,19 @@
 
-import Image from "next/image";
-import { fetchProductById } from "@/Server/fetchProducts.jsx";
 
+import { fetchProductById } from "@/Server/fetchProducts";
+import ProductDetailClient from "@/components/ProductDetailClient/ProductDetailClient";
 
 async function ProductDetail({ params }) {
 
     const product = await fetchProductById(params.productsid);
-
     
     if (!product) {
-        return <p>Producto no encontrado</p>;
+        return <p className="mt-10 text-center text-red-500">Producto no encontrado</p>;
     }
 
     return (
-        <div className="flex items-center justify-center m-auto my-10 bg-white border border-gray-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-             <h1>{product.nombre} </h1>
-             <Image src={product.imgUrl} alt={product.name} width={250} height={250} className="m-auto my-16" />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-                <h2 >{product?.color}</h2>
-                <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    <p>{product?.material}</p>
-                    <p>{product?.medidas}</p>
-                    <p>{product?.stock}</p>
-                </div>
-                <h3>{product.valor}</h3>
-            
-            </div>
-        </div>
+       
+        <ProductDetailClient product={product} />
     );
 }
 
