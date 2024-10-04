b78d9b86a77cd588cff2afb8d3dae3b1f6f62d5c
mbelen <beel.betanzoo@hotmail.com.ar>
2024-10-04 19:06:56 -0300

    cambios pagina about Belen T20

--- src/components/MapGoogle/MapGoogle.jsx
+++ src/components/MapGoogle/MapGoogle.jsx
@@ -5,12 +5,12 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { useMemo } from 'react';
 
 const MapGoogle = () => {
-  // Cargar los scripts de Google Maps con tu API key
+ 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, // Guarda tu API key en un archivo .env
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, 
   });
 
-  // Ubicación específica (Armenia, Quindío, Colombia)
+
   const center = useMemo(() => ({ lat: 4.5426, lng: -75.6682 }), []);
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -19,9 +19,9 @@ const MapGoogle = () => {
     <GoogleMap
       zoom={15}
       center={center}
-      mapContainerStyle={{ width: '100%', height: '400px' }} // Ajustar tamaño
+      mapContainerStyle={{ width: '70%', height: '400px' }} 
     >
-      {/* Marker en la ubicación deseada */}
+  
       <Marker position={center} />
     </GoogleMap>
   );
