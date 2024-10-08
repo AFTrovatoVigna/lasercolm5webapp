4a9f06c25f66361ca0edb959c1f9e9696bec0a5d
Galahamd <galahamd@hotmail.com>
2024-10-07 20:39:14 -0400

    Cambios Front

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -47,6 +47,7 @@ const RegisterForm = () => {
     try {
       const newuser = await register(dataUser);
       Swal.fire("Te registraste correctamente");
+      console.log ("res",newuser);
       const newCart = await CreateCart(newuser.id);
       console.log ("The new cart",newCart);
       console.log ("The new user id", newuser.id);
